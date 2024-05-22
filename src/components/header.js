8ba3f521824d7a09d2797b607680a1c4c0f5a2eb
Larissa <rissapins@gmail.com>
2024-05-22 19:58:30 -0300

    config

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,9 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import * as React from 'react';
 //import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
